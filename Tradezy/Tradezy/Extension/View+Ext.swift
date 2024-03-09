159357a0de3eae1d66608af0f56caa08d04e7ae9
kupriyanovNik <cucuprianov@icloud.com>
2024-03-09 21:26:47 +0300

    added shine effect

--- Tradezy/Tradezy/Extension/View+Ext.swift
+++ Tradezy/Tradezy/Extension/View+Ext.swift
@@ -32,3 +32,49 @@ extension View {
         frame(maxHeight: .infinity, alignment: .bottom)
     }
 }
+
+extension View {
+    func shineEffect(
+        _ trigger: Bool,
+        duration: CGFloat = 0.5,
+        clipShape: some Shape = .rect(cornerRadius: 10),
+        isRightToLeft: Bool = false
+    ) -> some View {
+        let gradient = LinearGradient(
+            colors: [
+                .clear,
+                .clear,
+                .white.opacity(0.1),
+                .white.opacity(0.5),
+                .white.opacity(1),
+                .white.opacity(0.5),
+                .white.opacity(0.1),
+                .clear,
+                .clear
+            ],
+            startPoint: .leading,
+            endPoint: .trailing
+        )
+
+        return self
+            .overlay {
+                GeometryReader {
+                    let size = $0.size
+                    let modifiedDuration = max(0.5, duration)
+
+                    Rectangle()
+                        .fill(gradient)
+                        .scaleEffect(y: 8)
+                        .keyframeAnimator(initialValue: 0.0, trigger: trigger) { content, progress in
+                            content
+                                .offset(x: -size.width + (progress * size.width * 2))
+                        } keyframes: { _ in
+                            CubicKeyframe(0, duration: 0.1)
+                            CubicKeyframe(1, duration: modifiedDuration)
+                        }
+                        .rotationEffect(.degrees(45))
+                        .scaleEffect(x: isRightToLeft ? -1 : 1)
+                }
+            }
+    }
+}
