2eb1417bea567c544a4604d65fcce029f9505cfa
kupriyanovNik <125807834+kupriyanovNik@users.noreply.github.com>
2024-03-09 19:12:39 +0300

    Add files via upload

---
+++ Tradezy/Tradezy/TradezyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradezyApp.swift
+//  Tradezy
+//
+//  Created by Никита Куприянов on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TradezyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
